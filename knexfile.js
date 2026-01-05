66b23bdb28e421fe71bf46b1a68bfaa71dce499c
yeabsira tewodros <yeabsiratewodros947@gmail.com>
2026-01-04 22:07:55 -0900

    edit my knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     client: 'pg',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: 'localhost',
       user: 'your_username',
       password: 'your_password',
       database: 'ethiopian_vote'
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     },
     migrations: {
       directory: './migrations'
